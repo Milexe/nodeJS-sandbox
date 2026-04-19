3fb8bd773957ad9ecb2195db50a962f0ffad2eff
Roman S <milexeuwu@gmail.com>
2026-04-19 15:51:52 +0300

    gif module fix, project structure update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,11 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { HttpModule } from '@nestjs/axios';
-import { GifController } from './gif/gif.controller';
-import { LoggerMiddleware } from './middleware/logger.middleware';
+import { Module } from '@nestjs/common';
+import { GifModule } from './gif/gif.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { DrinkModule } from './drink/drink.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [HttpModule, PrismaModule, DrinkModule, UsersModule, AuthModule],
-  controllers: [AppController, GifController],
-  providers: [AppService],
+  imports: [GifModule, PrismaModule, DrinkModule, UsersModule, AuthModule],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(LoggerMiddleware)
-      .forRoutes('gif');
-  }
-}
+export class AppModule {}
